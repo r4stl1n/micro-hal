676d8bdaad9af4fa3bd3d52ae5a01c21cd4c72ef
r4stl1n <royal.r4stl1n@gmail.com>
2022-05-09 17:15:01 -0700

    Clean up go rewrite files

--- code/pkg/champ/README.md
+++
@@ -1,5 +0,0 @@
-# LibChamp
-
-This is a port of the libchamp from the chvmp repository.
-
-Original code can be found here: https://github.com/chvmp/libchamp/
--- code/pkg/champ/cstructs/velocities.go
+++
@@ -1,8 +0,0 @@
-package cstructs
-
-import "github.com/r4stl1n/micro-hal/code/pkg/hmath"
-
-type Velocities struct {
-	Linear  hmath.Vec3
-	Angular hmath.Vec3
-}
